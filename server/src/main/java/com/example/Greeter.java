aa277ef047099b9c6db52818446a4a356256941f
mystique386 <43020441+mystique386@users.noreply.github.com>
2018-10-01 17:36:27 -0400

    fixed warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
   /**
-  * @param someone
-* @return someone   
+  * @param someone the name of person
+  * @return greeting string
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
